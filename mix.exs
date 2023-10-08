74d74ce5efa8ab6c41c9f5bf232e6b2acc06bc4e
Matthew Whitworth <matt@tallarium.com>
2023-10-08 12:09:53 +0100

    0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TeslaExtra.MixProject do
   def project do
     [
       app: :tesla_extra,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       description: "Additional Tesla middleware",
